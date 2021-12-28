6937c307aee12f7b9d4706ff61843623dab0fdfd
Jack Boettcher <jcboettcher@college.harvard.edu>
2021-12-28 00:44:57 -0600

    two queries

---
+++ src/graphql/Query/userByName1.js
@@ -0,0 +1,21 @@
+const User = require('../../models/User')
+
+const userByName1 = async (obj, { game, level, name, limit }, context) => {
+  try {
+    const users = await User.query()
+      .where({game, level, name})
+      .orderBy('score1','ASC')
+      .orderBy('score2','ASC')
+      .orderBy('createdAt','ASC')
+      .limit(limit)
+    return users
+  } catch (error) {
+    console.warn(error)
+    throw new Error('failed to get users by score1')
+  }
+}
+const resolver = {
+  Query: { userByName1 },
+}
+
+module.exports = resolver
---
+++ src/graphql/Query/userByName2.js
@@ -0,0 +1,21 @@
+const User = require('../../models/User')
+
+const userByName2 = async (obj, { game, level, name, limit }, context) => {
+  try {
+    const users = await User.query()
+      .where({game, level, name})
+      .orderBy('score2','ASC')
+      .orderBy('score1','ASC')
+      .orderBy('createdAt','ASC')
+      .limit(limit)
+    return users
+  } catch (error) {
+    console.warn(error)
+    throw new Error('failed to get users by score1')
+  }
+}
+const resolver = {
+  Query: { userByName2 },
+}
+
+module.exports = resolver
